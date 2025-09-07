712b6a07d1f29758094cc05e299e994692f514d4
MinhND69 <minhnd69@fpt.com>
2025-09-07 20:11:41 +0700

    chore: initial fe

---
+++ backoffice/asset/data/sidebar.tsx
@@ -0,0 +1,30 @@
+import {
+  COUNTRY_URL,
+  STATE_OR_PROVINCE_URL,
+} from '../../constants/Common';
+
+export const menu_customer_item_data = [
+  {
+    id: 1,
+    name: 'Customers',
+    link: '/customers',
+  },
+  {
+    id: 2,
+    name: 'Product Reviews',
+    link: '/reviews',
+  },
+];
+
+export const menu_location_item_data = [
+  {
+    id: 1,
+    name: 'Countries',
+    link: COUNTRY_URL,
+  },
+  {
+    id: 2,
+    name: 'State Or Provinces',
+    link: STATE_OR_PROVINCE_URL,
+  },
+];
---
+++ backoffice/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Chào mừng bạn đến với trang Home!</p>
+    </div>
+  );
+};
+
+export default Home;
