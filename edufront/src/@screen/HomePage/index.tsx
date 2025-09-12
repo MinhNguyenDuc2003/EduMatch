2b7ddf6bded32a06a7d3ab4be27da9236541e67a
JustinDo2909 <hieudeptraivodichh@gmail.com>
2025-09-13 00:44:41 +0700

    handleSubmit form

--- edufront/src/@screen/HomePage/index.tsx
+++ edufront/src/@screen/HomePage/index.tsx
@@ -1,61 +1,59 @@
 'use client';
 
-import { Container } from '@/lib/by/Div';
+import { CustomFormField } from '@/lib/cus/CustomFormField';
+import { Button } from '@/lib/cus/button';
 import Header from '@/pattern/core/Header';
 import LanguageSwitcher from '@/pattern/share/LanguageSwitcher';
 import { useTranslations } from 'next-intl';
 import Context from './seg/context';
-import { CustomFormField } from '@/lib/cus/CustomFormField';
-import { Button } from '@/lib/cus/button';
 
 export default function HomePage() {
   const t = useTranslations('home');
-  
+
   return (
     <Context.Provider>
       <Context.Consumer>
-        {({ data, methods : { watch, getValues, setValue},meds  }) => {
+        {({ data, methods: { watch, getValues, setValue, handleSubmit }, meds }) => {
           return (
             <>
-              <h1 className='text-3xl text-red-600'>{t('title')}</h1>
+              <h1 className="text-3xl text-red-600">{t('title')}</h1>
               <Header />
               <CustomFormField
-              name='Fields.User.name'
-              label='Name'
-              placeholder='Enter something...'
-              className='flex'
-              labelClassName='mr-4 w-32'
-              inputClassName='flex-1'
+                name="Fields.User.name"
+                label="Name"
+                placeholder="Enter something..."
+                className="flex"
+                labelClassName="mr-4 w-32"
+                inputClassName="flex-1"
               />
               <CustomFormField
-              name='Fields.User.age'
-              label='Age'
-              placeholder='Enter something...'
-              className='flex'
-              labelClassName='mr-4 w-32'
-              inputClassName='flex-1'
+                name="Fields.User.age"
+                label="Age"
+                placeholder="Enter something..."
+                className="flex"
+                labelClassName="mr-4 w-32"
+                inputClassName="flex-1"
               />
               <CustomFormField
-              name='Fields.User.gmail'
-              label='Gmail'
-              placeholder='Enter something...'
-              className='flex'
-              labelClassName='mr-4 w-32'
-              inputClassName='flex-1'
+                name="Fields.User.gmail"
+                label="Gmail"
+                placeholder="Enter something..."
+                className="flex"
+                labelClassName="mr-4 w-32"
+                inputClassName="flex-1"
               />
               <CustomFormField
-              name='Fields.User.description'
-              label='Description'
-              placeholder='Enter something...'
-              className='flex'
-              labelClassName='mr-4 w-32'
-              inputClassName='flex-1'
+                name="Fields.User.description"
+                label="Description"
+                placeholder="Enter something..."
+                className="flex"
+                labelClassName="mr-4 w-32"
+                inputClassName="flex-1"
               />
               {watch('Fields.User.name')}
               {watch('Fields.User.age')}
-              <Button label='Push data to N8n' onClick={() => meds.onPushDataToN8n()}/>
-               <LanguageSwitcher />
-               
+              <Button label="Push data to N8n" onClick={handleSubmit(meds.onPushDataToN8n)} />
+              <LanguageSwitcher />
             </>
           );
         }}
