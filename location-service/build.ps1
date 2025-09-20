a66490454081e26cc3c61224018eb2de19bef297
MinhND69 <minhnd69@fpt.com>
2025-09-20 20:55:29 +0700

    devops: setup harbor registry

---
+++ location-service/build.ps1
@@ -0,0 +1,9 @@
+$REGISTRY = "registry.edumatch.space"
+$PROJECT  = "edumatch"
+$SERVICE  = "location"
+$VERSION  = "1.0.0"
+
+docker build -t "$($SERVICE):$($VERSION)" .
+docker tag "$($SERVICE):$($VERSION)" "$($REGISTRY)/$($PROJECT)/$($SERVICE):$($VERSION)"
+docker login $REGISTRY
+docker push "$($REGISTRY)/$($PROJECT)/$($SERVICE):$($VERSION)"
