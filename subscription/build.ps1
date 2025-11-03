e08c653c77cc0f6e3ac1b61d5c1b7429c374213e
MinhNguyenDuc2003 <ducm40877@gmail.com>
2025-11-03 23:48:51 +0700

    feat: add subscription

---
+++ subscription/build.ps1
@@ -0,0 +1,9 @@
+$REGISTRY = "registry.edumatch.space"
+$PROJECT  = "edumatch"
+$SERVICE  = "subscription"
+$VERSION  = "1.0.0"
+
+docker build -t "$($SERVICE):$($VERSION)" .
+docker tag "$($SERVICE):$($VERSION)" "$($REGISTRY)/$($PROJECT)/$($SERVICE):$($VERSION)"
+docker login $REGISTRY
+docker push "$($REGISTRY)/$($PROJECT)/$($SERVICE):$($VERSION)"
