4e2427a8a629a1645f339c7c50fc468304c2d3dc
MinhNguyenDuc2003 <ducm40877@gmail.com>
2025-11-10 22:53:34 +0700

    fix: version fe

--- edufront/docker-build.ps1
+++ edufront/docker-build.ps1
@@ -1,6 +1,6 @@
 docker stop edumatch_edufront-nextjs_1
 docker rm edumatch_edufront-nextjs_1
-docker rmi registry.edumatch.space/edumatch/edufront-nextjs:1.1.0
-docker pull registry.edumatch.space/edumatch/edufront-nextjs:1.1.0
+docker rmi registry.edumatch.space/edumatch/edufront-nextjs:1.0.0
+docker pull registry.edumatch.space/edumatch/edufront-nextjs:1.0.0
 cd ..
 docker-compose up -d edufront-nextjs
