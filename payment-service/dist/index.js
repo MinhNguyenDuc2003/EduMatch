6845b07eb5c42c8602a7bdec5d584f01721241ed
MinhNguyenDuc2003 <ducm40877@gmail.com>
2025-11-17 19:52:24 +0700

    feat: delete cors

--- payment-service/dist/index.js
+++ payment-service/dist/index.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const paymentRouter_1 = __importDefault(require("./paymentRouter"));
@@ -18,7 +17,6 @@ app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
